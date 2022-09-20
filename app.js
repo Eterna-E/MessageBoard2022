46d2f7c13b3b1a8d3a577af35dfd50edcd23aac5
ZhenHao <zhenhao@TangChuandeMacBook-Air.local>
2022-09-20 15:02:41 +0800

    純nodeJS 留言板完成

--- app.js
+++ app.js
@@ -4,43 +4,25 @@ var url = require('url')
 var template = require('art-template')
 
 var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345678"
-});
-var comments ;
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM MessageBoard.Comments", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        comments = JSON.stringify(result);
-        comments = JSON.parse(comments);
-        console.log(comments);
-    });
-});
 
+function createConnection(){
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "MessageBoard"
+  });
+  return con;
+}
 
-
-// 每次重開服務時，預先保存的留言(可省略)
-// var comments = [
-//   {
-//     name: '小白',
-//     message: 'Hello',
-//     dateTime: '2021-5-27 18:20:54'
-//   },
-//   {
-//     name: '小黑',
-//     message: 'There',
-//     dateTime: '2021-5-27 18:20:54'
-//   }
-// ]
+var connection = null;
 
 http.createServer(function (req, res) {
-    var parseObj = url.parse(req.url, true)
-    
-    var pathname = parseObj.pathname
+    var parseObj = url.parse(req.url, true);
+    // console.log(parseObj);
+    var pathname = parseObj.pathname;
+    var postId = parseObj.query.id;
+    // console.log(pathname)
 
     // 路由邏輯
     if (pathname === '/') {
@@ -49,11 +31,25 @@ http.createServer(function (req, res) {
         if (err) {
           return res.end('Loading index page failed.')
         }
+        connection = createConnection();
+
+        var comments ;
+        connection.connect(function(err) {
+            if (err) throw err;
+        });
+        connection.query("SELECT * FROM Comments ORDER BY post_time DESC", function (err, result, fields) {
+          if (err) throw err;
+          // console.log(result);
+          comments = JSON.stringify(result);
+          comments = JSON.parse(comments);
+          
+          // console.log(comments)
+          connection.end();
+          // 因為 data 是二進制，所以須轉為 string
+          var htmlStr = template.render(data.toString(), { comments: comments });
+          res.end(htmlStr);
+        });
         
-        
-        // 因為 data 是二進制，所以須轉為 string
-        var htmlStr = template.render(data.toString(), { comments: comments })
-        res.end(htmlStr)
       })
     }
     // Post page
@@ -81,22 +77,24 @@ http.createServer(function (req, res) {
       // 取得 submit 的 data
       // comment = query: [Object: null prototype] { name: 'Leon', message: 'Say something' }
       var comment = parseObj.query;
+      var name = comment.name;
+      var message = comment.message;
 
-      console.log(comment);
-      
-      // 取得目前日期時間 (用於紀錄 submit 的時間)
-      // today =  2021-05-28T18:40:03.622Z
-      var today = new Date();
-      
-      var date = today.getFullYear() + '-' + (today.getMonth() + 1 ) + '-' + today.getDate();
-      var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTime = date + ' ' + time;
+      // console.log(comment);
 
-      // dateTime = '2021-5-28 21:33:55'
-      comment.dateTime = dateTime
+      connection = createConnection();
 
-      // 將 comment 放到 comments 內 (unshift 代表新 submit 的內容出現在列表最上面)
-      comments.unshift(comment)
+      connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+      var sql = `INSERT INTO Comments (name, message) VALUES ('${name}', '${message}')`;
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+        
+      });
+      // connection.end();
 
       // 設定狀態碼為 302 (網頁重定向)，用於網頁自動跳轉
       res.statusCode = 302
@@ -104,6 +102,111 @@ http.createServer(function (req, res) {
       res.setHeader('Location', '/')
       res.end()
     }
+
+    else if (pathname === '/update' ) {
+      
+      // data = ./views/post.html
+      fs.readFile('./views/update.html', function (err, data) {
+        if (err) {
+          return res.end('Loading post page failed.')
+        }
+        // var htmlStr = template.render(data.toString(), {
+        //   name: "comments[0].name"
+        // });
+        // console.log(htmlStr);
+        // res.end(htmlStr);
+        connection = createConnection();
+
+        var comments ;
+        connection.connect(function(err) {
+            if (err) throw err;
+        });
+        // console.log(postId);
+        connection.query(`SELECT * FROM Comments WHERE id='${postId}'`, function (err, result, fields) {
+          if (err) throw err;
+          // console.log(result);
+          comments = JSON.stringify(result);
+          comments = JSON.parse(comments);
+          
+          // console.log(comments)
+          // console.log(comments[0].name)
+          connection.end();
+          // 因為 data 是二進制，所以須轉為 string
+
+          var htmlStr = template.render(data.toString(), {
+            id: comments[0].id,
+            name: comments[0].name,
+            message: comments[0].message
+          });
+          // console.log(htmlStr);
+          res.end(htmlStr);
+          
+        });
+        
+      })
+    }
+
+    else if (pathname === '/submit-update') {
+
+      // 取得 submit 的 data
+      // comment = query: [Object: null prototype] { name: 'Leon', message: 'Say something' }
+      var comment = parseObj.query;
+      var name = comment.name;
+      var message = comment.message;
+      var postId = comment.id;
+
+      // console.log(comment);
+
+      connection = createConnection();
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+      var sql = `UPDATE Comments SET name='${name}', message='${message}' WHERE id='${postId}'`;
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record Updated");
+        
+      });
+      // connection.end();
+
+      // 設定狀態碼為 302 (網頁重定向)，用於網頁自動跳轉
+      res.statusCode = 302
+      // 跳轉至首頁
+      res.setHeader('Location', '/')
+      res.end()
+    }
+
+    else if (pathname === '/delete') {
+
+      // 取得 submit 的 data
+      // comment = query: [Object: null prototype] { name: 'Leon', message: 'Say something' }
+      var postId = parseObj.query.id;
+
+      // console.log(comment);
+
+      connection = createConnection();
+
+      connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+      var sql = `DELETE FROM Comments WHERE id='${postId}'`;
+      connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record Deleted");
+        
+      });
+      // connection.end();
+
+      // 設定狀態碼為 302 (網頁重定向)，用於網頁自動跳轉
+      res.statusCode = 302
+      // 跳轉至首頁
+      res.setHeader('Location', '/')
+      res.end()
+    }
+
     // 設定 page 404 
     else {
       fs.readFile('./views/404.html', function (err, data) {
